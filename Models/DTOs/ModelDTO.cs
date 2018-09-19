e92079a4ba00ee5d38cabbf10455fabb830fcb84
Vigdís Birna Þorsteinsdóttir <vigdis16@ru.is>
2018-09-19 15:38:10 +0000

    fixing errors

--- Models/DTOs/ModelDTO.cs
+++ Models/DTOs/ModelDTO.cs
@@ -1,6 +1,6 @@
 namespace TinySoldiers.Models.DTOs
 {
-    public class ModelDetailsDTO
+    public class ModelDTO
     {
         public int Id { get; set; }
         public string Name { get; set; }
